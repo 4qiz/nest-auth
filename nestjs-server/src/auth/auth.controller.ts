9d7fd2f544748dd6deb6d8b2bf79b5ded3bbd8a5
4qiz <egor.antipin.1995mail.ru@gmail.com>
2025-01-13 20:50:05 +0000

    add redis insight

--- nestjs-server/src/auth/auth.controller.ts
+++ nestjs-server/src/auth/auth.controller.ts
@@ -1,7 +1,17 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common'
+import {
+	Body,
+	Controller,
+	HttpCode,
+	HttpStatus,
+	Post,
+	Req,
+	Res
+} from '@nestjs/common'
 import { Recaptcha } from '@nestlab/google-recaptcha'
+import { Request, Response } from 'express'
 
 import { AuthService } from './auth.service'
+import { LoginDto } from './dto/login.dto'
 import { RegisterDto } from './dto/register.dto'
 
 @Controller('auth')
@@ -11,7 +21,23 @@ export class AuthController {
 	//@Recaptcha()
 	@Post('register')
 	@HttpCode(HttpStatus.OK)
-	public async register(@Body() dto: RegisterDto) {
-		return this.authService.register(dto)
+	public async register(@Req() req: Request, @Body() dto: RegisterDto) {
+		return this.authService.register(req, dto)
+	}
+
+	//@Recaptcha()
+	@Post('login')
+	@HttpCode(HttpStatus.OK)
+	public async login(@Req() req: Request, @Body() dto: LoginDto) {
+		return this.authService.login(req, dto)
+	}
+
+	@Post('logout')
+	@HttpCode(HttpStatus.OK)
+	public async logout(
+		@Req() req: Request,
+		@Res({ passthrough: true }) res: Response
+	) {
+		return this.authService.logout(req, res)
 	}
 }
--- nestjs-server/src/auth/auth.service.ts
+++ nestjs-server/src/auth/auth.service.ts
@@ -1,19 +1,30 @@
-import { ConflictException, Injectable } from '@nestjs/common'
+import {
+	ConflictException,
+	Injectable,
+	InternalServerErrorException,
+	NotFoundException,
+	UnauthorizedException
+} from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 import { AuthMethod, User } from '@prisma/__generated__'
+import { verify } from 'argon2'
+import { Request, Response } from 'express'
 
 import { PrismaService } from '@/prisma/prisma.service'
 import { UserService } from '@/user/user.service'
 
+import { LoginDto } from './dto/login.dto'
 import { RegisterDto } from './dto/register.dto'
 
 @Injectable()
 export class AuthService {
 	public constructor(
 		private readonly prismaService: PrismaService,
-		private readonly userService: UserService
+		private readonly userService: UserService,
+		private readonly configService: ConfigService
 	) {}
 
-	public async register(dto: RegisterDto) {
+	public async register(req: Request, dto: RegisterDto) {
 		const isExists = await this.userService.findByEmail(dto.email)
 
 		if (isExists) {
@@ -31,18 +42,68 @@ export class AuthService {
 			false
 		)
 
-		return this.saveSession(newUser)
+		return this.saveSession(req, newUser)
 		return {
 			message:
 				'Вы успешно зарегистрировались. Пожалуйста, подтвердите ваш email. Сообщение было отправлено на ваш почтовый адрес.'
 		}
 	}
 
-	public async login() {}
+	public async login(req: Request, dto: LoginDto) {
+		const user = await this.userService.findByEmail(dto.email)
 
-	public async logout() {}
+		if (!user || !user.password) {
+			throw new UnauthorizedException(
+				'Неверный пароль. Пожалуйста, попробуйте еще раз или восстановите пароль, если забыли его.'
+			)
+		}
 
-	public async saveSession(user: User) {
-		console.log('- save session -')
+		const isValidPassword = await verify(user.password, dto.password)
+
+		if (!isValidPassword) {
+			throw new UnauthorizedException(
+				'Неверный пароль. Пожалуйста, попробуйте еще раз или восстановите пароль, если забыли его.'
+			)
+		}
+
+		return this.saveSession(req, user)
+	}
+
+	public async logout(req: Request, res: Response): Promise<void> {
+		return new Promise((resolve, reject) => {
+			req.session.destroy(err => {
+				if (err) {
+					return reject(
+						new InternalServerErrorException(
+							'Не удалось завершить сессию. Возможно, возникла проблема с сервером или сессия уже была завершена.'
+						)
+					)
+				}
+				res.clearCookie(
+					this.configService.getOrThrow<string>('SESSION_NAME')
+				)
+				resolve()
+			})
+		})
+	}
+
+	public async saveSession(req: Request, user: User) {
+		return new Promise((resolve, reject) => {
+			req.session.userId = user.userId
+
+			req.session.save(err => {
+				if (err) {
+					return reject(
+						new InternalServerErrorException(
+							'Не удалось сохранить сессию. Проверьте, правильно ли настроены параметры сессии.'
+						)
+					)
+				}
+
+				resolve({
+					user
+				})
+			})
+		})
 	}
 }
